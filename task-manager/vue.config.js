dca4bfe801b579c024f0cfef72907d58eaa5ca5d
Dean Berg <144251180+DeanB27@users.noreply.github.com>
2025-04-22 21:45:56 -0400

    Update vue.config.js

--- task-manager/vue.config.js
+++ task-manager/vue.config.js
@@ -1,7 +1,6 @@
 // vue.config.js
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/task-manager/' // Replace with your repository name
-    : '/',
-  outputDir: 'dist'
+    ? '/term-project-dean_aaron/' 
+    : '/'
 }
